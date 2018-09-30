3df62bdccd52718939981bfa70f3c6533d993f78
setseed42 <erikosmund@gmail.com>
2018-10-01 00:14:22 +0200

    Use cleaner docker-compose syntax

--- startDev.sh
+++ startDev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose up -d --build
-docker exec -it "$(docker ps -aqf 'name=notebook')" /bin/sh -c "jupyter notebook list"
+docker-compose exec notebook jupyter notebook list
 docker-compose logs -f app
